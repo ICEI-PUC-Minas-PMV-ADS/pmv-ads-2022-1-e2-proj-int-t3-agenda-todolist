830dc41385102e5e13c979abb7de231605d36c41
viniciusfortino <vinicius.fortino@gmail.com>
2022-06-22 23:09:00 -0300

    cors

--- src/agenda-api/agenda-api/Startup.cs
+++ src/agenda-api/agenda-api/Startup.cs
@@ -53,6 +53,14 @@ namespace agenda_api
 
             app.UseRouting();
 
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
