da4a65865b44e983246ffca9110e6474132fbf0f
viniciusfortino <vinicius.fortino@gmail.com>
2022-06-22 22:59:28 -0300

    cors

--- src/agenda-api/agenda-api/Startup.cs
+++ src/agenda-api/agenda-api/Startup.cs
@@ -27,6 +27,7 @@ namespace agenda_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
 
             services.AddScoped<ITaskService, TaskService>();
